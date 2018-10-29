08275d525ec5ec0190ff1ebed527690542f0f790
ymeysa25 <40119560+ymeysa25@users.noreply.github.com>
2018-10-29 07:48:14 +0700

    Modul2
    Tugas Praktikum Modul 2

---
+++ Modul2/TugasMDP-master/AplikasiModul2Kel38 - Copy/app/src/main/java/com/example/nadia/aplikasimodul2kel38/AboutActivity.java
@@ -0,0 +1,29 @@
+package com.example.nadia.aplikasimodul2kel38;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public void onBackPressed() {
+        back();
+    }
+
+    private void back() {
+        Intent reg = new Intent(this, MainActivity.class);
+        startActivity(reg);
+        finish();
+    }
+}
